6c255a90b60b3bc6d488c2f068d5f719b867062f
Andree Jimenez <andree_jimenez@outlook.com>
2025-04-29 11:34:37 -0600

    Deploy github pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  base : '/HooBank/', 
+  // Replace with your actual GitHub repo name
+  build : {
+    outDir: 'docs'
+  },
 })
